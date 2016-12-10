83002493487268f2e6818654958c5deaa2d0e5e3
leoWang <492260726@qq.com>
2016-12-10 17:04:17 +0800

    123

--- app/index.js
+++ app/index.js
@@ -1,17 +1,7 @@
 import React,{Component} from 'react';
 import { render } from 'react-dom';
-<<<<<<< HEAD
  import { App } from './containers';
 
 
 let root = document.getElementById('app');
 render( <App />, root );
-=======
-// import { App } from './containers';
-import {FilterableProductTable} from './components';
-
-
-
-let root = document.getElementById('app');
-render( <FilterableProductTable />, root );
->>>>>>> 7e7f7527e417af68806e922fc1d7ba99ff46195d
